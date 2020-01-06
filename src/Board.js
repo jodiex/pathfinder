81654666ceeaf08e503dd4bfbf4d7b046de47036
Jodie Xiang <jodie.xiang144@gmail.com>
2020-01-06 15:42:26 -0500

    updated state calls

--- src/Board.js
+++ src/Board.js
@@ -36,19 +36,19 @@ class Board extends React.Component {
     visualize() {
         var curr = this.state.startNode;
         var end = this.state.endNode;
-        curr.distance = 0;
+        curr.setState({distance: 0});
         var visited = [];
         var unvisited = getAllNodes();
         while (unvisited.length > 0){
-            if (curr.row === end.row && curr.col === end.col) {
+            if (curr.state.row === end.state.row && curr.state.col === end.state.col) {
                 return visited;
             }
-            if (curr.isWall){
+            if (curr.state.isWall){
                 continue;
             }
+            curr.setState({isVisited: true});
             visited.push(curr);
-            curr.isVisited = true;
-            updateNeighbours(curr); // add + 1 to the distance of its neighbours
+            updateNeighbours(curr); // add + 1 to the distance of its neighbours and set prevNode to curr
             unvisited = sortByDistance(unvisited);
             curr = unvisited.shift();
         }
@@ -56,65 +56,72 @@ class Board extends React.Component {
     updateNeighbours(curr){
         var r = curr.row;
         var c = curr.col;
-        var newGrid = [];
         var oldGrid = this.state.grid;
+        var newGrid = oldGrid;
         var neighbour = null;
-        // if (r < HEIGHT) {
-        //     neighbour = oldGrid[r+1][c];
-        //     if (!neighbour.isVisited && !neighbour.isWall) {
-        //         neighbour.distance = curr.distance + 1;
-        //         neighbour.prevNode = curr;
-        //     }
-        // }
-        // if (r > 0) {
-        //     neighbour = oldGrid[r-1][c];
-        //     if (!neighbour.isVisited && !neighbour.isWall) {
-        //         neighbour.distance = curr.distance + 1;
-        //         neighbour.prevNode = curr;
-        //     }
-        // }
-        // if (c < WIDTH) {
-        //     neighbour = oldGrid[r][c+1];
-        //     if (!neighbour.isVisited && !neighbour.isWall) {
-        //         neighbour.distance = curr.distance + 1;
-        //         neighbour.prevNode = curr;
-        //     }
-        // }
-        // if (c > 0) {
-        //     neighbour = oldGrid[r][c-1];
-        //     if (!neighbour.isVisited && !neighbour.isWall) {
-        //         neighbour.distance = curr.distance + 1;
-        //         neighbour.prevNode = curr;
-        //     }
-        // }
-        for (let i = 0; i < HEIGHT; i++){
-            if (i < r-1 && i > r+1) {
-                var row = oldGrid[r];
-                newGrid.push(row);
+        if (r < HEIGHT) {
+            neighbour = oldGrid[r+1][c];
+            if (!neighbour.state.isVisited && !neighbour.state.isWall) {
+                neighbour.setState({
+                    distance: curr.state.distance + 1,
+                    prevNode: curr,
+                });
             }
-            else { // i == r-1 OR i == r+1 OR i == r
-                var row = [];
-                for(let j = 0; j < WIDTH; j++) {
-                    if (((j == c-1 || j == c+1) && i == r) || ((i == r-1 || i == r+1) && j == c)) {
-                        var neighbour = oldGrid[i][j];
-                        if (!neighbour.isVisited && !neighbour.isWall) { //this won't work i think because Node is now a react component and isVisited, etc. are in state
-                            neighbour.distance = curr.distance + 1;
-                            neighbour.prevNode = curr;
-                        }
-                        row.push(neighbour);
-                    }
-                    else {
-                        row.push(oldGrid[i][j]);
-                    }
-                newGrid.push(row);
-                }
+            newGrid[r+1][c] = neighbour;
+        }
+        if (r > 0) {
+            neighbour = oldGrid[r-1][c];
+            if (!neighbour.isVisited && !neighbour.isWall) {
+                neighbour.distance = curr.distance + 1;
+                neighbour.prevNode = curr;
             }
         }
-        this.setState({grid: newGrid});
+        if (c < WIDTH) {
+            neighbour = oldGrid[r][c+1];
+            if (!neighbour.isVisited && !neighbour.isWall) {
+                neighbour.distance = curr.distance + 1;
+                neighbour.prevNode = curr;
+            }
+        }
+        if (c > 0) {
+            neighbour = oldGrid[r][c-1];
+            if (!neighbour.isVisited && !neighbour.isWall) {
+                neighbour.distance = curr.distance + 1;
+                neighbour.prevNode = curr;
+            }
+        }
+
+
+        // for (let i = 0; i < HEIGHT; i++){
+        //     if (i < r-1 && i > r+1) {
+        //         var row = oldGrid[r];
+        //         newGrid.push(row);
+        //     }
+        //     else { // i == r-1 OR i == r+1 OR i == r
+        //         var row = [];
+        //         for(let j = 0; j < WIDTH; j++) {
+        //             if (((j == c-1 || j == c+1) && i == r) || ((i == r-1 || i == r+1) && j == c)) {
+        //                 var neighbour = oldGrid[i][j];
+        //                 if (!neighbour.state.isVisited && !neighbour.state.isWall) { //this won't work i think because Node is now a react component and isVisited, etc. are in state
+        //                     neighbour.setState({
+        //                         distance: curr.state.distance + 1,
+        //                         prevNode: curr,
+        //                     });
+        //                 }
+        //                 row.push(neighbour);
+        //             }
+        //             else {
+        //                 row.push(oldGrid[i][j]);
+        //             }
+        //         newGrid.push(row);
+        //         }
+        //     }
+        // }
+        // this.setState({grid: newGrid});
     }
 
     sortByDistance(arr) {
-        arr.sort(nodeGreaterThan(a,b) { return a.distance - b.distance; });
+        arr.sort(nodeGreaterThan(a,b) { return a.state.distance - b.state.distance; });
         return arr;
     }
     componentDidMount() {
